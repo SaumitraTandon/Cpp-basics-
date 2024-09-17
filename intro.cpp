939f6b475ec8e175e687687f444fb778ddabeac9
Saumitra Tandon <hellosaumitra@gmail.com>
2024-09-17 16:39:44 +0530

    Initial Commit

---
+++ intro.cpp
@@ -0,0 +1,13 @@
+#include<iostream> // iostream is kind of a file that contain the code that defines cout
+using namespace std; // We want to use cout of the std name face
+
+int main() {
+    
+    cout << "Namaste Duniya" << endl; // << is to write ; endl is end line ; ; is like punctuation for the compiler
+    int a = 123;
+    cout << a << endl;
+
+    int size = sizeof(a);
+    cout << "Size of a is : "<< size << endl;
+    cout << "My enemies are many, my equals are none." << endl;
+}
